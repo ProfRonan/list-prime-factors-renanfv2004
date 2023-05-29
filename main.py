ce8678149bc8516095c98903a705a940b8466b87
renanfv2004 <128155898+renanfv2004@users.noreply.github.com>
2023-05-28 21:43:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-"""Módulo com funções."""
-
-
 def is_prime(number: int) -> bool:
     """Retorna True se o número for primo e False caso contrário."""
-
-    return False
-
+    numero = number
+    a = 1
+    while True:
+        a = a + 1
+        if numero == 1:
+            return False
+        elif numero <= 0:
+            return False
+        elif numero % a == 0 and a != numero:
+            return False
+        elif a == numero:
+            return True
 
 def list_prime_factors(number: int) -> list[int]:
     """Retorna uma lista com os fatores primos de cada número da lista fornecida."""
+    prime_factors = []
+    for i in range(2, number + 1):
+        if is_prime(i):
+            while number % i == 0:
+                prime_factors.append(i)
+                number //= i
 
-    return []
+    return prime_factors
